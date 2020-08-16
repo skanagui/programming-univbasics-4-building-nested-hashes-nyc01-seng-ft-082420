d2037f2ac260d3c7a46549de387fdac91b31481f
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-16 19:52:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,30 +1,32 @@
-def base_hash { 
-  railroads: {}
-  
-}
+def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-  
-
+  {
+    railroads: {}
+  }
 end
 
 def monopoly_with_second_tier
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
   {
-  railroads: {
-    pieces: 4,
-    rent_in_dollars: {},
-    names: {}
-    
+    railroads: {
+      pieces: 4,
+      rent_in_dollars: {},
+      names: {}
+    }
   }
-}
+end
+
+def monopoly_with_third_tier
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 
-end
-
-def monopoly_with_third_tier {
+  {
     railroads: {
       pieces: 4,
       rent_in_dollars: {
@@ -41,14 +43,14 @@ def monopoly_with_third_tier {
       }
     }
   }
+end
+
+def monopoly_with_fourth_tier
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
-end
-
-def monopoly_with_fourth_tier {
+  {
     railroads: {
       pieces: 4,
       rent_in_dollars: {
