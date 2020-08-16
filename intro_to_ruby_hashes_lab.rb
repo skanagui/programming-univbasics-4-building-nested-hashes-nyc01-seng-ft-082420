c2c9b0b661c0f7a9a16189f0e3a1c5cb7f4cf6f2
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-16 19:43:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,10 @@ def monopoly_with_third_tier {
   railroads: {
     pieces: 4,
     rent_in_dollars: {
-      :one
+      one_piece_owned: 25,
+      two_pieces_owned: 50,
+      three_eices_owned: 100,
+      four_pieces_owned: 200
     },
     names: {},
     
